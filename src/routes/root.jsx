15797eb85921f60992f84f5bd27900256dc38455
Upanshu Parekh <54222922+blc5-33@users.noreply.github.com>
2024-03-15 12:27:33 -0500

    Basic landing page done

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,11 +1,17 @@
 import musaicLogo from '/musaic-logo.svg'
+import spotifyLogo from '/spotify-logo.svg'
+import './root.css'
 
 function Root() {
     return (
-      <body>
-        <img src={musaicLogo}/>
-        <p>Hello!</p>
-      </body>
+      <>
+        <img src={musaicLogo} style={{height: 165}}/>
+        <p className='font-secondary'>Your story through your tunes.</p>
+        <div className='green-button'>
+          <img src={spotifyLogo}/>
+          <p className='font-primary'>Sign in to Start Exploring</p>
+        </div>
+      </>
     )
 }
 
