0364d5e52bcfb3ae220424541c3a252e0b9e58ae
Upanshu Parekh <54222922+blc5-33@users.noreply.github.com>
2024-03-18 19:31:51 -0500

    Commented which parts to dynamically update with Spotify query

--- src/routes/visualization/visualization-page.jsx
+++ src/routes/visualization/visualization-page.jsx
@@ -3,15 +3,19 @@ import Dial from '../../components/dial/dial'
 
 function VisualizationPage() {
   return (
+    // TODO: make a variable of currently selected month
     <>
       <Navbar />
       <div className='vis-grid'>
         <h3 className='font-primary item-year'>2023</h3>
+        {/* TODO: Update selected month text */}
         <h3 className='font-primary item-month'>Click on a month!</h3>
         <div className='item-dial'>
+          {/* TODO: Make Dial update the month (pass in as ref argument?) */}
           <Dial />
         </div>
         <div className='item-most'>
+          {/* TODO: Update top artists/songs based on month selected */}
           <h3>Top Artists:</h3>
           <div className='image-row'>
             <p className='sample'>?</p>
