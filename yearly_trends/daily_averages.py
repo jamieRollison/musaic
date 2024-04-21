36c1884d4810759fd4a2c7f6a9310fbcf9a1e567
anpatel204 <anpatel204@gmail.com>
2024-04-20 23:59:07 -0500

    Calculate variance & std error for data

--- yearly_trends/daily_averages.py
+++ yearly_trends/daily_averages.py
@@ -7,7 +7,7 @@ import os
 # Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), 
 # while tracks with low valence sound more negative (e.g. sad, depressed, angry).
 
-with open('data_processing/final_data_records_melanie.json', 'r') as file:
+with open('data_processing/final_data_records_jamie.json', 'r') as file:
     # Load the JSON data into a Python dictionary
     data = json.load(file)
 
@@ -51,6 +51,7 @@ for track in data:
 
 # write avg values for all attributes per day to JSON file
 average_daily_attributes = {}
+error_bars_daily_attributes = {}
 for day, attributes in attributes_by_date.items():
     average_daily_attributes[day] = {
         'valence_avg' : sum(attributes['valence_avg'])/len(attributes['valence_avg']),
@@ -60,34 +61,71 @@ for day, attributes in attributes_by_date.items():
         'tempo_avg': sum(attributes['tempo_avg'])/len(attributes['tempo_avg']),
         'speechiness_avg': sum(attributes['speechiness_avg'])/len(attributes['speechiness_avg'])
     }
-with open('yearly_trends/daily_average_attributes_melanie.json', 'w') as output_file:
+with open('yearly_trends/daily_average_attributes_jamie.json', 'w') as output_file:
     json.dump(average_daily_attributes, output_file, indent=4)
 
 
+for day, attributes in attributes_by_date.items():
+    valence_avg = sum(attributes['valence_avg'])/(len(attributes['valence_avg']))
+    danceability_avg = sum(attributes['danceability_avg'])/(len(attributes['danceability_avg']))
+    energy_avg = sum((attributes['energy_avg']))/(len(attributes['energy_avg']))
+    acousticness_avg = sum(attributes['acousticness_avg'])/(len(attributes['acousticness_avg']))
+    tempo_avg = sum(attributes['tempo_avg'])/(len(attributes['tempo_avg']))
+    speechiness_avg = sum(attributes['speechiness_avg'])/(len(attributes['speechiness_avg']))
+
+    valence_stderr = np.std(attributes['valence_avg'])/np.sqrt(len(attributes['valence_avg']))
+    danceability_stderr = np.std(attributes['danceability_avg'])/np.sqrt(len(attributes['danceability_avg']))
+    energy_stderr = np.std((attributes['energy_avg']))/np.sqrt(len(attributes['energy_avg']))
+    acousticness_stderr = np.std(attributes['acousticness_avg'])/np.sqrt(len(attributes['acousticness_avg']))
+    tempo_stderr = np.std(attributes['tempo_avg'])/np.sqrt(len(attributes['tempo_avg']))
+    # speechiness_stderr = np.std(attributes['speechiness_avg'])/np.sqrt((attributes['speechiness_avg']))
+    error_bars_daily_attributes[day] = {
+        'valence_error_bounds' : (valence_avg - valence_stderr, valence_avg + valence_stderr),
+        'danceability_error_bounds': (danceability_avg - danceability_stderr, danceability_avg + danceability_stderr),
+        'energy_error_bounds' : (energy_avg - energy_stderr, energy_avg + energy_stderr),
+        'acousticness_error_bounds': (acousticness_avg - acousticness_stderr, acousticness_avg + acousticness_stderr),
+        'tempo_error_bounds': (tempo_avg - tempo_stderr, tempo_avg + tempo_stderr),
+        # 'speechiness_error_bounds': (speechiness_avg - speechiness_stderr, speechiness_avg + speechiness_stderr)
+    }
+
+with open('yearly_trends/daily_stderror_attributes_jamie.json', 'w') as stderr_file:
+    json.dump(error_bars_daily_attributes, stderr_file, indent=4)
 
 average_daily_valence = {}
+variance_daily_valence = {}
 for day, valences in valence_by_date.items():
     average_daily_valence[day] = (sum(valences)/len(valences))
+    variance_daily_valence[day] = np.var(valences)
 
 average_daily_danceability = {}
+variance_daily_danceability = {}
 for day, danceabilities in danceability_by_date.items():
     average_daily_danceability[day] = (sum(danceabilities)/len(danceabilities))
+    variance_daily_danceability[day] = np.var(danceabilities)
 
 average_daily_energy = {}
+variance_daily_energy = {}
 for day, energies in energy_by_date.items():
     average_daily_energy[day] = (sum(energies)/len(energies))
+    variance_daily_energy[day] = np.var(energies)
 
 average_daily_acousticness = {}
+variance_daily_acousticness = {}
 for day, acoustics in acousticness_by_date.items():
     average_daily_acousticness[day] = (sum(acoustics)/len(acoustics))
+    variance_daily_acousticness[day] = np.var(acoustics)
 
 average_daily_tempo = {}
+variance_daily_tempo = {}
 for day, tempos in tempo_by_date.items():
     average_daily_tempo[day] = (sum(tempos)/len(tempos))
+    variance_daily_tempo[day] = np.var(tempos)
 
 average_daily_speechiness = {}
+variance_daily_speechiness = {}
 for day, speeches in speechiness_by_date.items():
     average_daily_speechiness[day] = (sum(speeches)/len(speeches))
+    variance_daily_speechiness[day] = np.var(speeches)
 
 
 # # Write the average daily valence values to a file
@@ -116,27 +154,22 @@ for day, speeches in speechiness_by_date.items():
 #         output_file.write(f"{day}: {average_speechiness}\n")
 
 
-# for day, average_tempo in average_daily_valence.items():    # FOR TESTING
-#     print(f"{day}, average valence: {average_valence}")
 
+# def convert_to_date(str_date):
+#     month, day = map(int, str_date.split('-'))
+#     return f"2023-{month}-{day}"
 
+# # Read data from the original JSON file
+# with open('/Users/anami/Documents/musaic/yearly_trends/daily_average_attributes_melanie.json', 'r') as file:
+#     dData = json.load(file)
 
+# # Convert string dates to JavaScript Date objects
+# new_data = {}
+# for key, value in dData.items():
+#     new_data[convert_to_date(key)] = value
 
-def convert_to_date(str_date):
-    month, day = map(int, str_date.split('-'))
-    return f"2023-{month}-{day}"
-
-# Read data from the original JSON file
-with open('/Users/anami/Documents/musaic/yearly_trends/daily_average_attributes_melanie.json', 'r') as file:
-    dData = json.load(file)
-
-# Convert string dates to JavaScript Date objects
-new_data = {}
-for key, value in dData.items():
-    new_data[convert_to_date(key)] = value
-
-# Write the updated data to a new JavaScript file
-with open('yearly_trends/date_formatted_melanie_dailydata.js', 'w') as file:
-    file.write('const newData = ')
-    json.dump(new_data, file, indent=4)
-    file.write(';')
+# # Write the updated data to a new JavaScript file
+# with open('yearly_trends/date_formatted_melanie_dailydata.js', 'w') as file:
+#     file.write('const newData = ')
+#     json.dump(new_data, file, indent=4)
+#     file.write(';')
